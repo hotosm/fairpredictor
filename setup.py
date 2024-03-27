b844ed7feb0a713de8308583660b7b632f6e2fbd
kshitijrajsharma <skshitizraj@gmail.com>
2024-03-27 19:54:21 +0545

    v0.0.33

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with io.open("README.md", encoding="utf-8") as f:
 
 setup(
     name="fairpredictor",
-    version="0.0.32",
+    version="0.0.33",
     url="https://github.com/kshitijrajsharma/fairpredictor",
     author="Kshitij Raj Sharma",
     author_email="skshitizraj@gmail.com",
