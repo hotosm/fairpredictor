323ce5ad7279f45a526c9f14380342ba20bdf90c
kshitijrajsharma <skshitizraj@gmail.com>
2024-09-16 21:23:39 +0545

    update payload for predictor

--- locust.py
+++ locust.py
@@ -8,24 +8,22 @@ class MyUser(HttpUser):
     def predict_api(self):
         payload = {
             "bbox": [
-                100.56228021333352,
-                13.685230854641182,
-                100.56383321235313,
-                13.685961853747969,
+                39.73467081522629,
+                -5.054039921565167,
+                39.73741203283951,
+                -5.05203875471145,
             ],
-            "checkpoint": "/mnt/efsmount/data/trainings/dataset_58/output/training_324/checkpoint.tflite",
-            "zoom_level": 20,
-            "source": "https://tiles.openaerialmap.org/6501a65c0906de000167e64d/0/6501a65c0906de000167e64e/{z}/{x}/{y}",
-            "use_josm_q": "false",
-            "merge_adjacent_polygons": "true",
-            "confidence": 50,
+            "model_id": "118",
+            "checkpoint": "/mnt/efsmount/data/trainings/dataset_184/output/training_417/checkpoint.tflite",
+            "zoom_level": 19,
+            "source": "https://tiles.openaerialmap.org/5d971f6ae2b1f300057cb312/0/5d971f6ae2b1f300057cb313/{z}/{x}/{y}",
+            "confidence": 90,
+            "use_josm_q": False,
             "max_angle_change": 15,
             "skew_tolerance": 15,
-            "tolerance": 0.5,
-            "area_threshold": 3,
-            "tile_overlap_distance": 0.15,
+            "tolerance": 0.3,
+            "area_threshold": 4,
         }
-
         headers = {"Content-Type": "application/json"}
 
         response = self.client.post("predict/", json=payload, headers=headers)
