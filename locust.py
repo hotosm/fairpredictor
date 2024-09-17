784da21f0c9457413c60e81d67090178cbb578fa
kshitijrajsharma <skshitizraj@gmail.com>
2024-09-17 09:32:54 +0545

    fair-prod locust payload

--- locust.py
+++ locust.py
@@ -8,15 +8,15 @@ class MyUser(HttpUser):
     def predict_api(self):
         payload = {
             "bbox": [
-                39.73467081522629,
-                -5.054039921565167,
-                39.73741203283951,
-                -5.05203875471145,
+                85.52111327648163,
+                27.632799326007426,
+                85.52421927452089,
+                27.634308247990774,
             ],
-            "model_id": "118",
-            "checkpoint": "/mnt/efsmount/data/trainings/dataset_184/output/training_417/checkpoint.tflite",
+            "model_id": "3",
+            "checkpoint": "/mnt/efsmount/data/trainings/dataset_3/output/training_22/checkpoint.tflite",
             "zoom_level": 19,
-            "source": "https://tiles.openaerialmap.org/5d971f6ae2b1f300057cb312/0/5d971f6ae2b1f300057cb313/{z}/{x}/{y}",
+            "source": "https://tiles.openaerialmap.org/64d3642319cb3a000147a5be/0/64d3642319cb3a000147a5bf/{z}/{x}/{y}",
             "confidence": 90,
             "use_josm_q": False,
             "max_angle_change": 15,
