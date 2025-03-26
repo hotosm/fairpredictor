7fd07bc6e6a807590d60a7e1720f63e0c677fb46
kshitijrajsharma <skshitizraj@gmail.com>
2025-03-26 23:43:49 +0100

    fix(default): adds default value for model checkpoitns and image url

--- predictor/__init__.py
+++ predictor/__init__.py
@@ -1,1 +1,8 @@
 from .app import predict
+
+DEFAULT_TMS_URL = "https://apps.kontur.io/raster-tiler/oam/mosaic/{z}/{x}/{y}.png"
+DEFAULT_YOLO_MODEL_V1 = "https://api-prod.fair.hotosm.org/api/v1/workspace/download/yolo/yolov8s_v1-seg.onnx"
+DEFAULT_RAMP_MODEL = (
+    "https://api-prod.fair.hotosm.org/api/v1/workspace/download/ramp/baseline.tflite"
+)
+DEFAULT_YOLO_MODEL_V2 = "https://api-prod.fair.hotosm.org/api/v1/workspace/download/yolo/yolov8s_v2-seg.onnx"
