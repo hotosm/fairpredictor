185173e164692fbb3a6695f4e66ef393f956f9ec
kshitijrajsharma <skshitizraj@gmail.com>
2025-05-07 19:13:49 +0200

    fix(bump): bumps recent version of predictor

--- test.py
+++ test.py
@@ -2,11 +2,15 @@ import asyncio
 
 from predictor import DEFAULT_OAM_TMS_MOSAIC, DEFAULT_RAMP_MODEL, predict
 
-bbox = [100.56228, 13.685230, 100.56383, 13.685961]
+bbox = [39.231538732285344, -6.809193620326397, 39.23551086950809, -6.808174352344608]
+model_link = "https://api-prod.fair.hotosm.org/api/v1/workspace/download/training_309/checkpoint.onnx"
+
+print("Running prediction...")
+
 asyncio.run(
     predict(
         bbox=bbox,
-        model_path=DEFAULT_RAMP_MODEL,
+        model_path=model_link,
         tms_url=DEFAULT_OAM_TMS_MOSAIC,
         zoom_level=20,
         orthogonalize=True,
